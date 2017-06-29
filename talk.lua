e874b26cf5c33c0d95114eff7668e8919529cd3d
leafi <leaf@leaf.sx>
2017-06-29 01:30:49 +0100

    talk: arrow!

--- talk.lua
+++ talk.lua
@@ -13,6 +13,17 @@ local talk = {
     left = 150,
     right = 100
   },
+  c1 = {255, 255, 255, 255},
+  c2dark = {52, 11, 74, 255},
+  c2light = {104, 22, 148, 255},
+  ctx = {
+    drag = 8,
+    head = 4,
+    hh = 8,
+    sz = 24,
+    x = 1840,
+    y = 1035,
+  },
   queue = {}
 }
 I.talk = talk
@@ -27,6 +38,21 @@ function talk:draw()
   if self.bg.bottomborder > 0 then
     util.drawrectv(0, 1080 - self.bg.bottomborder, 1920, self.bg.bottomborder, {0, 0, 0, self.bg.a2}, {0, 0, 0, self.bg.a3})
   end
+
+  local p1x = talk.ctx.x - talk.ctx.sz
+  local py = talk.ctx.y - talk.ctx.sz
+  local p2x = talk.ctx.x + talk.ctx.sz
+  local phy = py - talk.ctx.head
+
+  local pyd = py + talk.ctx.drag
+  local ctxyd = talk.ctx.y + talk.ctx.drag
+
+  love.graphics.setColor(talk.c2light)
+  love.graphics.polygon('fill', p1x, pyd, p1x, phy, p2x, phy, p2x, pyd, talk.ctx.x, ctxyd)
+
+  util.drawrectv(p1x, py - talk.ctx.head - talk.ctx.hh, talk.ctx.sz * 2, talk.ctx.hh, {talk.c1[1], talk.c1[2], talk.c1[3], 0}, talk.c1)
+  love.graphics.setColor(talk.c1)
+  love.graphics.polygon('fill', p1x, py, p1x, phy, p2x, phy, p2x, py, talk.ctx.x, talk.ctx.y)
 end
 
 function talk:update(dt)
